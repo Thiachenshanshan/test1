2d160a58d3fff3969ded2d16a617efe1afc6692f
Thia <42692513+Thiachenshanshan@users.noreply.github.com>
2020-08-25 10:59:13 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -1,7 +1,10 @@
 function drag(id){
     var el = document.getElementById(id)
+    var disX = 0;
+    var disY = 0;
     el.onmousedown = function(ev){
-        var disX = 0;
-        var disY = 0;
+        var ev = ev || window.event;
+        disX = ev.pageX - el.offsetLeft;
+        disY = ev.pageY - el.offsetTop;
     }
 }
