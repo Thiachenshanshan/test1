8c8e06eaa27d4e32702d74c2fd25aaa721cc6657
Thia <42692513+Thiachenshanshan@users.noreply.github.com>
2020-08-26 09:45:47 +0800

    changed2 drag.js

--- drag.js
+++ drag.js
@@ -6,5 +6,10 @@ function drag(id){
         var ev = ev || window.event;
         disX = ev.pageX - el.offsetLeft;
         disY = ev.pageY - el.offsetTop;
+        el.onmousemove = function(ev){
+            var ev = ev || window.event;
+            el.style.left = ev.clientX - disX + 'px';
+            el.style.top = ev.clientY - disY + 'px';
+        }
     }
 }
